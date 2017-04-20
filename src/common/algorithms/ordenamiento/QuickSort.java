1a42625e161cadd2de4e485a071ddda6707ed2fa
Antonio Hernandez <ahernandez@nearsoft.com>
2017-04-20 14:54:17 -0500

    Quick sort solution

--- src/common/algorithms/ordenamiento/QuickSort.java
+++ src/common/algorithms/ordenamiento/QuickSort.java
@@ -8,6 +8,33 @@ public class QuickSort {
      * @param array Array a ordenar
      */
     public static void ordenar(int[] array) {
-        // Implementacion del algoritmo QuickSort
+        quickSort(array, 0, array.length-1);
+    }
+
+    private static void quickSort(int[] array, int inicio, int fin) {
+        if(inicio < fin) {
+            int pIndex = particionar(array, inicio, fin);
+            quickSort(array, inicio, pIndex-1);
+            quickSort(array, pIndex+1, fin);
+        }
+    }
+
+    private static int particionar(int[] array, int inicio, int fin) {
+        int pivote = array[fin];
+        int pIndex = inicio;
+        for(int i=inicio; i <= fin-1; i++) {
+            if(array[i] <= pivote) {
+                swap(array, i, pIndex);
+                pIndex++;
+            }
+        }
+        swap(array, pIndex, fin);
+        return pIndex;
+    }
+
+    private static void swap(int[] array, int indice1, int indice2) {
+        int temp = array[indice1];
+        array[indice1] = array[indice2];
+        array[indice2] = temp;
     }
 }
