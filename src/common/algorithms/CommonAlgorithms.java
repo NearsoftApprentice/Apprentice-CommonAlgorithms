8c7b00a7ada44516c7946b9dd4b67cd01b57ee05
Antonio Hernandez <ahernandez@nearsoft.com>
2017-04-20 11:05:56 -0500

    [CommonAlgorithms] Base template

--- src/common/algorithms/CommonAlgorithms.java
+++ src/common/algorithms/CommonAlgorithms.java
@@ -8,15 +8,33 @@ import java.util.Arrays;
 public class CommonAlgorithms {
 
     public static void main(String[] args) {
-        int[] array = new int[]{9,8,7,6,5,4,3,2,1};
 
-        System.out.println("Initial: ");
-        printArray(array);
+        //Ordenamiento
+        int[] array1 = new int[]{9,8,7,6,5,4,3,2,1};
+        int[] array2 = new int[]{9,8,7,6,5,4,3,2,1};
 
-        Ordenamiento.QuickSort(array);
+        //Primer implementacion
+        System.out.println("Original: ");
+        printArray(array1);
+        //Ordenar array
+        System.out.println("Ordenado:");
+        printArray(array1);
 
-        System.out.println("Sorted:");
-        printArray(array);
+        //Segunda implementacion
+        System.out.println("Original: ");
+        printArray(array2);
+        //Ordenar array
+        System.out.println("Ordenado:");
+        printArray(array2);
+
+        //Busqueda
+        //int t = 5;
+        //int indice = Busqueda.metodo(array, t);
+        //System.out.println("Elemento " + t + " ubicado en indice " + indice);
+
+        //t = 7;
+        //indice = Busqueda.metodo(array, t);
+        //System.out.println("Elemento " + t + " ubicado en indice " + indice);
 
     }
 
