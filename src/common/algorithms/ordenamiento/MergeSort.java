b87d82267b71920afee8450e56f970084b311107
Antonio Hernandez <ahernandez@nearsoft.com>
2017-04-20 20:05:10 -0500

    Merge sort solution

--- src/common/algorithms/ordenamiento/MergeSort.java
+++ src/common/algorithms/ordenamiento/MergeSort.java
@@ -1,5 +1,7 @@
 package common.algorithms.ordenamiento;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
     /**
@@ -8,6 +10,57 @@ public class MergeSort {
      * @param array Array a ordenar
      */
     public static void ordenar(int[] array) {
-        // Implementacion del algoritmo MergeSort
+        int n = array.length;
+        if (n >= 2) {
+            int mid = n/2;
+            int[] L = new int[mid];
+            int[] R = new int[n-mid];
+
+            for(int i=0; i<mid; i++) {
+                L[i] = array[i];
+            }
+
+            for(int i=mid; i<n; i++) {
+                R[i-mid] = array[i];
+            }
+
+            System.out.println(Arrays.toString(L));
+            System.out.println(Arrays.toString(R));
+
+            ordenar(L);
+            ordenar(R);
+            merge(array, L, R);
+        }
+    }
+
+    private static void merge(int[] array, int[] L, int[] R) {
+        int nL = L.length;
+        int nR = R.length;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i <nL && j < nR) {
+            if(L[i] <= R[j]) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < nL) {
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < nR) {
+            array[k] = R[j];
+            j++;
+            k++;
+        }
     }
 }
